ce5d3abab8ca3faa6e881c2544c9bab9ebf6804e
R. Lemos <rslemos@gmail.com>
2019-07-08 09:48:56 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ let session = pl.create(1000);
 console.log('prolog', flex);
 
 session.consult(flex);
-session.query('likes(Y, X), likes(Y, Z).');
-session.answers(answer => console.log(answer));
+//session.query('horizontal(line(point(2,3),P)).');
+//session.query('crosswd(V1,V2,V3,H1,H2,H3).');
+//session.query('add(succ(succ(succ(0))),succ(succ(0)),R)');
+session.query('add(0,1,R)');
+session.answers(answer => console.log(pl.format_answer(answer)));
